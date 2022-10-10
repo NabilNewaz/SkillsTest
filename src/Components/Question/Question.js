7673c422dc98c08e1242ebbf79e02e2b1e284b7d
NabilNewaz <nabilnewaz@gmail.com>
2022-10-11 05:31:19 +0600

    eye btn to see correct ans added

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,9 +1,16 @@
-import { Card } from 'flowbite-react';
-import React from 'react';
+import { Alert, Card } from 'flowbite-react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ question }) => {
+    const [isActive, setIsActive] = useState(true);
+    const handleAnsView = event => {
+        // 👇️ toggle isActive state on click
+        setIsActive(current => !current);
+    };
     const right = () => toast.success("Your answer is correct 😊", {
         position: "top-right",
         autoClose: 5000,
@@ -28,9 +35,13 @@ const Question = ({ question }) => {
         <div className='mt-5'>
             <ToastContainer />
             <Card>
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                    {question.question.slice(3, -4)}
-                </h5>
+                <div className='flex justify-between'>
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white max-h-screen ">
+                        {question.question.slice(3, -4)}
+                    </h5>
+                    <FontAwesomeIcon onClick={handleAnsView} className={isActive ? '' : 'hidden'} icon={faEye} />
+                    <FontAwesomeIcon onClick={handleAnsView} className={isActive ? 'hidden' : ''} icon={faEyeSlash} />
+                </div>
                 {
                     question.options.map(option =>
                         <div onClick={option === question.correctAnswer ? right : wrong} className='cursor-pointer border-solid border-2 border-blue-600 hover:bg-blue-600 hover:text-white p-3 rounded'>
@@ -39,7 +50,19 @@ const Question = ({ question }) => {
                     )
                 }
             </Card>
-        </div >
+            <div className={isActive ? 'hidden' : ''}>
+                <Alert
+                    color="success"
+                    onDismiss={function onDismiss() { handleAnsView() }}>
+                    <span>
+                        <span className="font-medium">
+                            Correct Answer :
+                        </span>
+                        {' '}{question.correctAnswer}
+                    </span>
+                </Alert>
+            </div>
+        </div>
     );
 };
 
