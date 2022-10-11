201a09d36c85d88137aeb69d76664bb2a8b12d75
NabilNewaz <nabilnewaz@gmail.com>
2022-10-11 13:01:57 +0600

    statistics added

--- src/Components/statistics/statistics.js
+++ src/Components/statistics/statistics.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const QuizData = useLoaderData()
+    console.log(QuizData)
     return (
-        <div>
-            <h2>this is Statistics</h2>
+        <div className='lg:px-96 md:mt-5 px-0'>
+            <div className='hidden md:block'>
+                <ResponsiveContainer width={750} height={450}>
+                    <BarChart data={QuizData.data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="total" fill="#8884d8" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+            <div className='md:hidden block'>
+                <ResponsiveContainer width={320} height={350}>
+                    <BarChart data={QuizData.data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="total" fill="#8884d8" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     );
 };
