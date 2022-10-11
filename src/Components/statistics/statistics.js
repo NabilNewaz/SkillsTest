a2525fa235eeb71e74c7cf84ff1195f9caacea1e
NabilNewaz <nabilnewaz@gmail.com>
2022-10-11 14:04:40 +0600

    blog question added

--- src/Components/statistics/statistics.js
+++ src/Components/statistics/statistics.js
@@ -6,20 +6,8 @@ const Statistics = () => {
     const QuizData = useLoaderData()
     return (
         <div className='lg:px-96 md:mt-5 px-0'>
-            <div className='hidden md:block'>
-                <ResponsiveContainer width={750} height={450}>
-                    <BarChart data={QuizData.data}>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="total" fill="#8884d8" />
-                    </BarChart>
-                </ResponsiveContainer>
-            </div>
-            <div className='md:hidden block'>
-                <ResponsiveContainer width={320} height={350}>
+            <div>
+                <ResponsiveContainer width='92%' height={400}>
                     <BarChart data={QuizData.data}>
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="name" />
